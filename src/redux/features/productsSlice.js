f1b1f9e02d67f8d866fb951eeee0067fd6b3a151
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-02-16 18:49:47 +0100

    working on home page

--- src/redux/features/productsSlice.js
+++ src/redux/features/productsSlice.js
@@ -1,8 +1,23 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const url = '';
 
 const initialState = {
+  products: [],
+  isLoading: false,
+  isError: false,
   isSidebarOpen: false,
 };
+
+export const getProducts = createAsyncThunk('products/getAll', async () => {
+  try {
+    // GET URL
+    const { data } = await axios(url);
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 export const productsSlice = createSlice({
   name: 'products',
